67449593a5f282b07c02644dad81ed0beb025272
João Pedro Patrício do Nascimento <joaoppn00@gmail.com>
2022-09-25 21:46:28 -0300

    Aplicando o Form no Flask

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
-@app.route("/")
+entries = []
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    if request.method == "POST":
+        entry_exemplo = request.form.get("exemplo")
+        format_date = datetime.datetime.today().strftime("%d-%m-%y")
+        entries.append((entry_exemplo, format_date))
+
+    entries_with_date = [
+        (
+            entry[0],
+            entry[1],
+            datetime.datetime.strptime(entry[1], "%d-%m-%y").strftime("%b %d")
+        )
+        for entry in entries
+    ]
+
+    return render_template("index.html", entries=entries_with_date)
+
+
