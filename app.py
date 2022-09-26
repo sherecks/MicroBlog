230e6e1c13c53d17b48dbfbbf2dbe96b9784f7d1
João Pedro Patrício do Nascimento <joaoppn00@gmail.com>
2022-09-25 23:54:37 -0300

    Save

--- app.py
+++ app.py
@@ -2,29 +2,30 @@ import datetime
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient("mongodb+srv://Sherecks:wdJC51GNTvmpOdB0@sherecks.rqgpedc.mongodb.net/test")
+    app.db = client.Microblog
+    entries = []
 
-app = Flask(__name__)
-client = MongoClient("mongodb+srv://Sherecks:wdJC51GNTvmpOdB0@sherecks.rqgpedc.mongodb.net/test")
-app.db = client.Microblog
-entries = []
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        print([e for e in app.db.entries.find({})])
+        if request.method == "POST":
+            entry_exemplo = request.form.get("exemplo")
+            format_date = datetime.datetime.today().strftime("%d-%m-%y")
+            app.db.entries.insert_one({"exemplo": entry_exemplo, "date": format_date})
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    print([e for e in app.db.entries.find({})])
-    if request.method == "POST":
-        entry_exemplo = request.form.get("exemplo")
-        format_date = datetime.datetime.today().strftime("%d-%m-%y")
-        app.db.entries.insert_one({"exemplo": entry_exemplo, "date": format_date})
+        entries_with_date = [
+            (
+                entry["exemplo"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%d-%m-%y").strftime("%b %d")
+            )
+            for entry in app.db.entries.find({})
+        ]
 
-    entries_with_date = [
-        (
-            entry["exemplo"],
-            entry["date"],
-            datetime.datetime.strptime(entry["date"], "%d-%m-%y").strftime("%b %d")
-        )
-        for entry in app.db.entries.find({})
-    ]
+        return render_template("index.html", entries=entries_with_date)
 
-    return render_template("index.html", entries=entries_with_date)
-
+    return app
 
